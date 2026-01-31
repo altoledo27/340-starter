a22ee920d0c288b0141a59a47f5d898b1bac8f4f
altoledo27 <altoledoz27@gmail.com>
2026-01-31 15:23:17 -0600

    handle errors correction

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,9 +2,10 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const Util = require("../utilities/")
 //Router to build inventory classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/detail/:invId", invController.buildByInventoryId);
+router.get("/type/:classificationId", Util.handleErrors(invController.buildByClassificationId));
+router.get("/detail/:invId", Util.handleErrors(invController.buildByInventoryId));
 router.get("/intentional-500", (req, res, next) =>{err.status =500;})
 
 module.exports = router;
